675c037b11f76b0195aefd5375dbb0f132eec91b
marc310k <marcelo.koskur@gmail.com>
2022-05-20 16:15:53 -0300

    first commit

---
+++ ehDivisivel/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 21;
+        int num2 = 7;
+
+        if (num1 % num2 == 0) {
+            System.out.println("Eh divisivel");
+        } else {
+            System.out.println("Nao eh divisiel");
+        }
+
+
+    }
+}
