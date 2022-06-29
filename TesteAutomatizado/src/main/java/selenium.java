a2012e50f51661bba0e067965247a3eee379ebd6
marc310k <marcelo.koskur@gmail.com>
2022-06-29 17:25:36 -0300

    teste automatizado

---
+++ TesteAutomatizado/src/main/java/selenium.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class selenium {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:/driver/chromedriver.exe");
+        WebDriver driver;
+
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com");
+
+        WebElement campoUsuario = driver.findElement(By.id("user-name"));
+        campoUsuario.sendKeys("standard_user");
+
+        WebElement campoSenha = driver.findElement(By.id("password"));
+        campoSenha.sendKeys("secret_sauce");
+
+        WebElement botaoLogin = driver.findElement(By.id("login-button"));
+        botaoLogin.click();
+
+    }
+
+}
